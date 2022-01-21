31246111db991248e1de223252be3ce16fcb3922
Michael1796 <marshal.2015085@iit.ac.lk>
2022-01-21 18:27:00 +0530

    added cognito token management and serveless backend connection

--- src/services/auth-header.js
+++ src/services/auth-header.js
@@ -2,7 +2,7 @@ export default function authHeader() {
     const user = localStorage.getItem('idToken');
 
     if(user && user.token) {
-        return {  'x-access-token': user.token};
+        return {  'x-access-token': user.token , 'Access-Control-Allow-Origin': '*'};
     } else {
         return {};
     }
